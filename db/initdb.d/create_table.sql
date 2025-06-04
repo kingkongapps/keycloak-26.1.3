c0e233ea1787d122e61b13fe6b48aa92c3e1d6a5
inmo.kang <kangscom@naver.com>
2025-06-05 00:13:31 +0900

    first commit

---
+++ db/initdb.d/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sample (
+  id              INT       NOT NULL AUTO_INCREMENT,
+  name            VARCHAR(50)   NULL,
+  addr            VARCHAR(500)  NULL,
+  cell_phone      VARCHAR(500)  NULL,
+  PRIMARY KEY (id)
+);
---
+++ db/initdb.d/load_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO sample VALUES (null, '홍길동', '서울시 관악구', '010-1111-2222');
+INSERT INTO sample VALUES (null, '김길동', '서울시 관악구', '010-2222-2222');
+INSERT INTO sample VALUES (null, '강길동', '서울시 종로구', '010-3333-2222');
+INSERT INTO sample VALUES (null, '박길동', '서울시 종로구', '010-4444-2222');
+
